ad2811138a149049bb23846032829dca31838d9d
gaya-go <rkdi4512@gmail.com>
2026-02-25 15:03:11 +0000

    Week 8 Assignment completed.

---
+++ homework8_gaya_kim.py
@@ -0,0 +1,16 @@
+# Week8-Functions
+# Create a function that accepts a list of sandwich ingredients.
+# The function should have one parameter that can collect as many arguments as the function call provides.
+# The function should print a summary of the sandwich being ordered.
+def make_sandwich(*ingredients):
+    print('\nSandwich Order Summary:')
+    print('You ordered a sandwich with the following ingredients:')
+
+    for ingredient in ingredients:
+        print(f'- {ingredient}')
+
+# Call the function two times with a different number of arguments. 
+# First function call
+make_sandwich("Turkey", "Cheese", "Lettuce")
+# Seond function call
+make_sandwich("Salmon", "Onion", "Tomato", "Lettuce", "Cream cheese")
